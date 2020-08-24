c33147e1bbe5dba6eaf5a7ce813443a1e2440e24
HritikThapa7 <hritkmjth@gmail.com>
2020-08-24 16:56:34 +0545

    Profile and selling functions added

--- Backend/store/models.py
+++ Backend/store/models.py
@@ -4,39 +4,43 @@ from django.shortcuts import reverse
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+
 class Customer(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE,null=True,blank=True)
-    name=models.CharField(max_length=200,null=True)
-    email=models.CharField(max_length=200,null=True)
-    phone_no=models.CharField(max_length=200,null=True)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    phone_no = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return self.name
 
+
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    name = models.CharField(max_length=200,null=True)
+    name = models.CharField(max_length=200, null=True)
     category = models.CharField(max_length=200)
     price = models.IntegerField()
     description = models.TextField()
-    negotiation = models.BooleanField(default=False,null=True,blank=False)
-    img = models.ImageField(upload_to='pics',null=True,blank=True)
+    negotiation = models.BooleanField(default=False, null=True, blank=False)
+    img = models.ImageField(upload_to='pics', null=True, blank=True)
     contact_info = models.TextField()
-    urgent=models.BooleanField(default=False,blank=True)
-   
-    def  get_absolute_url(self):
-        return reverse("Description",kwargs={"pk" : self.pk})
-    
+    urgent = models.BooleanField(default=False, blank=True)
+
+    def get_absolute_url(self):
+        return reverse("Description", kwargs={"pk": self.pk})
+
     def get_add_to_whishlist_url(self):
-         return reverse("add_to_whishlist",kwargs={"pk" : self.pk})
-    
+        return reverse("add_to_whishlist", kwargs={"pk": self.pk})
+
     @property
     def imageUrl(self):
         try:
-            url= self.img.url
-        
+            url = self.img.url
+
         except:
-            url =''
+            url = ''
         return url
 
 
@@ -49,7 +53,6 @@ class Product(models.Model):
 #     transaction_id= models.CharField(max_length=150,null=True)
 
 
-
 # class Whishlist_item(models.Model):
 #     user=models.ForeignKey(User, on_delete=models.CASCADE,null=True)
 #     product =models.ForeignKey(Product,on_delete=models.SET_NULL,blank=True,null=True)
@@ -59,10 +62,11 @@ class Product(models.Model):
 
 
 class WishlistItem(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     ordered = models.BooleanField(default=False)
     item = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
+
 
 class Wishlist(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -72,8 +76,9 @@ class Wishlist(models.Model):
     ordered_date = models.DateTimeField()
     ordered = models.BooleanField(default=False)
 
+
 class Comment(models.Model):
-    post = models.ForeignKey(Product,null=True, on_delete=models.CASCADE)
+    post = models.ForeignKey(Product, null=True, on_delete=models.CASCADE)
     user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     reply = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
     content = models.CharField(max_length=160)
