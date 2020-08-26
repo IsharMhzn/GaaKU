f23a1880ab7f3de1f4537315a9a9d099f9936a0c
Kushal Manandhar <manandharkushal1@gmail.com>
2020-08-26 11:11:49 +0545

    Pulling

--- Backend/Home/views.py
+++ Backend/Home/views.py
@@ -5,6 +5,9 @@ from Account.models import Notification, NotificationCount, Updates
 import random
 # Create your views here.
 def index(request):
+    notifs = []
+    newNotif = None
+    allproducts = []
     if request.user.is_authenticated:
         notifc = NotificationCount.objects.get_or_create(user=request.user)[0]
         newNotif = not notifc.seen
@@ -19,31 +22,31 @@ def index(request):
                         continue
                 if n.post.user == request.user:
                     notifs.append(n)
-        
         try:
             u = Updates.objects.get(user=request.user)
-            allproducts = Product.objects.all()[::-1]
+            allpros = Product.objects.all()[::-1]
+            for p in allpros:
+                if p.timestamp:
+                    if u.timestamp < p.timestamp:
+                        if p.user != request.user:
+                            allproducts.append(p)
         except:
             allproducts = None
-
     products=Product.objects.all()[::-1][:10]
     featuredProducts=Product.objects.all()
     negotiableProducts=Product.objects.filter(negotiation=True)
-    
     randomFeaturedProducts=([random.choice(featuredProducts) for i in range(len(featuredProducts))])
     resultFeatuedProducts= []
     for item in randomFeaturedProducts:
         if not item in resultFeatuedProducts:
             resultFeatuedProducts.append(item)
     resultFeatuedProducts=resultFeatuedProducts[:len(resultFeatuedProducts)//4*4]
-
     randomNegotiableProducts=[random.choice(negotiableProducts) for i in range(len(negotiableProducts))]
     resultNegoitableProduct=[]
     for item in randomNegotiableProducts:
         if not item in resultNegoitableProduct:
             resultNegoitableProduct.append(item)
     resultNegoitableProduct=resultNegoitableProduct[:10]
-
     context = {'products':products, 
                'featuredProducts':resultFeatuedProducts,
                'negotiable':resultNegoitableProduct,
