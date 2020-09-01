bdd4981491afd02a80361cfd4cf01ea4b14c1b0a
HritikThapa7 <hritkmjth@gmail.com>
2020-09-01 18:09:15 +0545

    sept1

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -85,9 +85,9 @@ WSGI_APPLICATION = 'Backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gaaku',
-        'USER': 'ishar',
-        'PASSWORD': 'postgres',
+        'NAME': 'Test3',
+        'USER': 'postgres',
+        'PASSWORD': '4265063h',
         'HOST': 'localhost'
     }
 }
